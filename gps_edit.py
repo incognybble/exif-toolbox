9eb24be37543a0bcf53219bbd77eefee749bbcfd
incognybble <incognybble@users.noreply.github.com>
2016-02-21 23:20:02 +1100

    Added function to strip GPS from individual file. Debugged remove_gps_folder()

--- gps_edit.py
+++ gps_edit.py
@@ -1,6 +1,6 @@
 # By: incognybble
 # Created: 7th Feb 2016
-# Last modified: 14th Feb 2016
+# Last modified: 21st Feb 2016
 
 import os
 
@@ -33,23 +33,12 @@ def remove_gps_exif(photo):
 
     return (exif_bytes, gps)
 
-def add_exif(photo, exif_bytes):
-    """Add EXIF to EXIF-less photo. Used for copying modified EXIF to
-    existing image without overwriting any images. Useful if the existing
-    image is different than the original.
-
-    photo: File location of photo to receive EXIF.
-    exif_bytes: EXIF data.
-    """
-    
-    piexif.insert(exif_bytes, photo)
-
 def modify_exif(photo, exif_bytes, new_photo=None):
     """Save EXIF to either current file or make a new image.
 
     photo: Original file location.
     exif_bytes: EXIF data.
-    new_photo: New file location. Optional.
+    new_photo: Modified file location. Optional.
     """
     
     if new_photo == None:
@@ -58,14 +47,14 @@ def modify_exif(photo, exif_bytes, new_photo=None):
     im = Image.open(photo)
     im.save(new_photo, "jpeg", exif=exif_bytes)
 
-def remove_gps_folder(folder, new_folder=None, original=True, status_print = False):
+def remove_gps_folder(folder, new_folder=None, original=True, status_print=False):
     """Remove GPS from EXIF of all jpgs in a folder.
 
     folder: Location of original folder.
     new_folder: Location of folder for modified images. Optional.
     original: Flag indicating whether to use original image data. Optional.
-        Default is True. Alternative would be to insert EXIF into an
-        existing image.
+        Default is True. Alternative would be to replace EXIF of existing
+        images with matching filenames.
     status_print: Flag indicating whether to print current progress.
         Optional. Default is False.
     """
@@ -80,18 +69,39 @@ def remove_gps_folder(folder, new_folder=None, original=True, status_print = Fal
             if (os.path.splitext(photo)[1]).lower() == ".jpg":
                 old_file = os.path.join(folder, photo)
                 new_file = os.path.join(new_folder, photo)
-                (exif_bytes, gps) = remove_gps_exif(old_file)
 
-                if orginal == False:
-                    add_exif(new_file, exif_bytes)
+                gps = None
+
+                if original == False:
+                    (exif_bytes, gps) = remove_gps_exif(old_file)
+                    piexif.insert(exif_bytes, new_file)
                 else:
-                    modify_exif(new_file, exif_bytes)
+                    gps = remove_gps_photo(old_file, new_file)
 
                 if status_print == True:
                     if gps:
                         print photo
                     else:
+                        print gps
                         print photo + " - Nope!"
+
+def remove_gps_photo(photo, new_photo=None):
+    """Remove GPS from EXIF of a given photo. Returns flag indicating if
+    original file had GPS EXIF data.
+
+    photo: Location of original photo.
+    new_photo: Location for modified photo. Optional. Default is to overwrite
+        the original photo.
+    gps: Flag indicating if original file had GPS EXIF data.
+    """
+    
+    if new_photo == None:
+        new_photo = photo
+        
+    (exif_bytes, gps) = remove_gps_exif(photo)
+    modify_exif(photo, exif_bytes, new_photo)
+
+    return gps
 
 if __name__ == "__main__":
     pass
